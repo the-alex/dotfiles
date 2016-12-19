60bff823c796e5400a212db697b6d52d13b5c2a1
Alex Chojnacki <thealex@umich.edu>
2016-12-19 00:47:52 -0500

    updated fish config so welcome message is empty string

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -2,11 +2,7 @@
 # alias rm="rmtrash"
 alias rme="rm -riv"
 
-# Base16 Shell
-#if status --is-interactive
-    #eval sh $HOME/.config/base16-shell/scripts/base16-default-dark.sh
-#end
-
 # added by Anaconda3 2.4.1 installer
 set PATH "/Users/theLoaner/anaconda/bin" $PATH
 
+set fish_greeting ""
