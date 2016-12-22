8ed13bfff28ad3073f36d7152d6bd9e009326498
Alex Chojnacki <thealex@umich.edu>
2016-12-22 17:21:30 -0500

    Recusive symlink (maybe) fix and zsh movement++

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@
 
 ########## Variables
 
-dir=$(pwd)/dotfiles                    # dotfiles directory
+dir=~/dotfiles/dotfiles   # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="$(ls dotfiles)"    # list of files/folders to symlink in homedir
+files="$(ls $dir)"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -30,3 +30,4 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
